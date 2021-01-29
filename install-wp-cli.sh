b4083aaf311f7da57d153fa65b7d52b31d047095
Wiloke <contact.wilcity@gmail.com>
2021-01-29 22:52:25 +0700

    Added Ubuntu
    Signed-off-by: Wiloke <contact.wilcity@gmail.com>

--- install-wp-cli.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download
-./wp-cli.phar config create --dbname=wordpress --dbuser=root --dbhost=127.0.0.1 --dbpass='' --skip-check
-./wp-cli.phar core install --admin_name=admin --admin_password=admin --admin_email=admin@example.com --url=http://localhost:8888 --title=WordPress
