92b7a6cbe8d9c7f5d7e9812ab88499556fc3234c
Wiloke <contact.wilcity@gmail.com>
2021-01-29 16:52:41 +0700

    Added files and folders
    Signed-off-by: Wiloke <contact.wilcity@gmail.com>

---
+++ wiloke-circleci.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Wiloke Circleci
+ * Plugin URI: https://wiloke.com
+ * Author: Wiloke
+ * Author URI: https://wiloke.com
+ * Description: Integrating Circle Ci to WP
+ * Author: 1.0
+ */
+
+
