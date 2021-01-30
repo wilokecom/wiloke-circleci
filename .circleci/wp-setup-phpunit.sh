254c18979429a8fe0e3acd3f45203f71975122ec
Wiloke <contact.wilcity@gmail.com>
2021-01-30 19:21:24 +0700

    Improved phpunittest
    Signed-off-by: Wiloke <contact.wilcity@gmail.com>

--- .circleci/wp-setup-phpunit.sh
+++ .circleci/wp-setup-phpunit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd ${PLUGIN_PATH} &&./vendor/bin/wilokecli make:unittest plugins ${PLUGIN_NAME} \
+cd ${PLUGIN_PATH} && ./vendor/bin/wilokecli make:unittest plugins ${PLUGIN_NAME} \
 --homeurl=${WP_URL} --rb=${REST_BASE} --namespace=${TEST_NAMESPACE} --admin_username=${WP_ADMIN} --admin_password=${WP_ADMIN}
