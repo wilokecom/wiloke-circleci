c8d425ac8c6931345dee34f6182fdfea478034f7
Wiloke <contact.wilcity@gmail.com>
2021-01-30 22:53:51 +0700

    Improved phpunittest
    Signed-off-by: Wiloke <contact.wilcity@gmail.com>

--- tests/User/Controllers/PostControllerTest.php
+++ tests/User/Controllers/PostControllerTest.php
@@ -9,11 +9,11 @@ class PostControllerTest extends TestCase
 {
 	use HTTP;
 	public function testPosts() {
-		var_export(function_exists('get_current_user_id'));die;
-		// $aResponse = $this->setUserLogin('admin')->restGET('posts');
-		// $this->assertNotEmpty($aResponse);
-		// foreach ($aResponse as $aPost) {
-		// 	$this->assertArrayHasKey('title', $aPost);
-		// }
+		// var_export(function_exists('get_current_user_id'));die;
+		$aResponse = $this->setUserLogin('admin')->restGET('posts');
+		$this->assertNotEmpty($aResponse);
+		foreach ($aResponse as $aPost) {
+			$this->assertArrayHasKey('title', $aPost);
+		}
 	}
 }
