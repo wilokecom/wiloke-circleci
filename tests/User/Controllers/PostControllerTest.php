5effd88159d96d1f62b2ae30bf4218d877ed5e37
Wiloke <contact.wilcity@gmail.com>
2021-02-01 23:37:09 +0700

    Sua loi createApplicationPassword
    Signed-off-by: Wiloke <contact.wilcity@gmail.com>

--- tests/User/Controllers/PostControllerTest.php
+++ tests/User/Controllers/PostControllerTest.php
@@ -7,6 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 class PostControllerTest extends CommonController
 {
+	protected function createApplicationPassword()
+	{
+		$phpUnitTest = dirname(dirname(dirname(plugin_dir_path(__FILE__)))) . '/phpunit.xml';
+		$content = file_get_contents($phpUnitTest);
+
+		if (strpos($content, 'ADMIN_AUTH_PASS_VALUE') !== false) {
+			$aResponse = \WP_Application_Passwords::create_new_application_password($this->getAdminId(), [
+				'name' => 'My App'
+			]);
+
+			$content = str_replace('ADMIN_AUTH_PASS_VALUE', $aResponse[0], $content);
+			file_put_contents($phpUnitTest, $content);
+		}
+	}
+	
 	public function testPosts()
 	{
 		$this->createApplicationPassword();
