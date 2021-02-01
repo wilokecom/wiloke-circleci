e8d6dd2ad69f8e12bf58a5870bcc700608307042
Wiloke <contact.wilcity@gmail.com>
2021-02-02 00:15:32 +0700

    Sua loi createApplicationPassword
    Signed-off-by: Wiloke <contact.wilcity@gmail.com>

--- .circleci/install-wp-cli.sh
+++ .circleci/install-wp-cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-./wp-cli.phar core download
-./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbhost=${DB_HOST} --dbpass=${DB_PASSWORD} --skip-check
+./wp-cli.phar core download --version=latest
+./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbhost=${DB_HOST} --dbpass=${DB_PASSWORD} --extra-php --skip-check
